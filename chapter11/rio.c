8386cce9593d0d74bab613319b0381870977832c
zhiqiang.shi <zhiqiang.shi@opay-inc.com>
2020-06-19 13:58:53 +0800

    add rio_readn

---
+++ chapter11/rio.c
@@ -0,0 +1,25 @@
+
+
+int main(void) {
+
+}
+
+// 无缓冲的输入函数
+ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR)
+                nread = 0;
+            else
+                return -1;
+        } else if (nread == 0)
+            break;
+        nleft -= nread;
+        bufp += nread;
+    }
+    return (n - nleft);
+}
