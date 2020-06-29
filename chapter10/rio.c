1441c01fd40d048af711cd86da0295cb3d27ecf8
zhiqiang.shi <zhiqiang.shi@opay-inc.com>
2020-06-29 14:06:13 +0800

    add function rio_writen to rio.c

--- chapter10/rio.c
+++ chapter10/rio.c
@@ -23,3 +23,22 @@ ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
     }
     return (n - nleft);
 }
+
+// 无缓冲的输出函数
+ssize_t rio_writen(int fd, void *usrfbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nwrite = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)
+                nwritten = 0;
+            else
+                return -1;
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
