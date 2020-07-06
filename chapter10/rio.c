e333ec009e957ae9c6bcce30ec9167532731dde6
zhiqiang.shi <zhiqiang.shi@opay-inc.com>
2020-07-06 14:10:12 +0800

    add readlineb readnb function

--- chapter10/rio.c
+++ chapter10/rio.c
@@ -81,3 +81,43 @@ static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {
     rp->cnt -= cnt;
     return cnt;
 }
+
+// linux read line 缓冲版本
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+    int n, rc;
+    char c, *bufp = usrbuf;
+
+    for (n = 1; n < maxlen; n++) {
+        if ((rc = rio_read(rp, &c, 1)) == 1) {
+            *bufp++ = c;
+            if (c == '\n') {
+                n++;
+                break;
+            }
+        } else if (rc == 0) {
+            if (n == 1)
+                return 0;
+            else:
+                break;
+        } else
+            return -1;
+    }
+    *bufp = 0;
+    return n-1;
+}
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nread = rio_read(rp, bufp, nleft)) < 0)
+            return -1;
+        else if (nread == 0)
+            break;
+        nleft -= nread;
+        bufp += nread;
+    }
+    return (n - nleft);
+}
